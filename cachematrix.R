e073db0cd5d66f29253e48b6ca733619fb6f9283
chiku248 <amit.pradhan1992@gmail.com>
2016-07-05 22:56:58 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,12 @@
++# The following are two functions that involve matrices. 
+ +# The first function stores the matrix in a cache allowing it to be retrieved 
+ +# at a later point in another function. 
+ +# The second function uses the matrix stored in the first function to find it's 
+ +# inverse. 
+  
+ -## Write a short comment describing this function
+ +# In order to use these functions, you first need to create a square matrix.
+ +# Here are two examples of square matrices that I have created
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
@@ -12,6 +21,11 @@ makeCacheMatrix <- function(x = matrix()) {
        setInverse = setInverse,
        getInverse = getInverse)
 }
+-## Write a short comment describing this function
+ +# This function will find the inverse of the matrix stored in makeCacheMatrix 
+ +# function. 
+ +# The input for cacheSolve has to be makeCacheMatrix or a value stored of 
+ +# makeCacheMatrix.
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   inv <- x$getInverse()
